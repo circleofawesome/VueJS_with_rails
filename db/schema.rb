f8d6b4d15ddfceb57eb85eec65abc80b4829cd53
Samiul Anwar <samiul.anwar@gmail.com>
2017-08-08 11:54:31 -0400

    basic app working in rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170808155020) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.boolean "manager"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
